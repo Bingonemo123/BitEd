d2597e91aa9ae14c7f3566a294a46c5c5d6106c1
Nikoloz Tvildiani <ww.bingonemo@gmail.com>
2023-03-12 12:17:23 +0400

    migrations

--- bitedMainProject/settings.py
+++ bitedMainProject/settings.py
@@ -145,11 +145,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = Path.home()/'static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    # BASE_DIR / "static",
+    BASE_DIR / "static",
     '/var/www/static/',
 ]
 
