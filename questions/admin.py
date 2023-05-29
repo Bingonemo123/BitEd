b717e97f950695880368d8a4ed81766ed1d97c3a
Nikoloz Tvildiani <ww.bingonemo@gmail.com>
2023-05-30 02:40:57 +0400

    make faster admin questions

--- questions/admin.py
+++ questions/admin.py
@@ -4,5 +4,9 @@ from .models import Question
 from .models import QuestionChoice
 # Register your models here.
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    raw_id_fields = ['correct_choice', 'owner', 'next_question_in_group']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(QuestionChoice)
