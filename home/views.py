49597048ad6d50b261e169899932fe0235b7e051
Ahmed Askar <ahmedeamain@gmail.com>
2024-02-24 09:55:14 +0200

    finalll

--- home/views.py
+++ home/views.py
@@ -7,7 +7,7 @@ from profile.models import Profile
 from django.shortcuts import render
 from django.http import HttpResponse
 from home.ajax import is_ajax
-
+import os
 
 from map.views import mapview_for_home
 
@@ -36,3 +36,33 @@ def dark_mode(request):
         request.session['theme'] = json.loads(request.body)['theme']
 
         return HttpResponse(status=204)
+    
+import os
+import os
+from django.shortcuts import redirect, HttpResponse
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+
+def google_auth(request):
+    try:
+        # Instantiate the OAuth2Client
+        client = OAuth2Client(
+            request=request,
+            consumer_key=os.getenv('GOOGLE_OAUTH2_CLIENT_ID'),
+            consumer_secret=os.getenv('GOOGLE_OAUTH2_CLIENT_SECRET'),
+            access_token_method='POST',
+            access_token_url='https://accounts.google.com/o/oauth2/token',
+            callback_url='http://127.0.0.1:8000/accounts/google/login/callback/',
+            scope=['email', 'profile'],
+            scope_delimiter=' ',
+        )
+
+        # Redirect users to Google's authentication URL
+        authorization_url = 'https://accounts.google.com/o/oauth2/auth'
+        extra_params = {}
+        redirect_url = client.get_redirect_url(authorization_url, extra_params)
+        return redirect(redirect_url)
+    except Exception as e:
+        print(e)
+        # Handle the error gracefully (e.g., display a friendly error message to the user)
+        return HttpResponse("An error occurred during authentication. Please try again later.", status=500)
